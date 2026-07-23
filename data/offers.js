e3ac5f548a00152a439c6d624f4da9abdc23da48
Md Asif <your-email@example.com>
2026-07-23 08:10:07 +0600

    Dynamic Offers

---
+++ data/offers.js
@@ -0,0 +1,33 @@
+const offers = [
+
+{
+    id:1,
+    title:"BC Game Welcome Bonus",
+    description:"নতুন একাউন্ট খুলে Welcome Bonus নিন।",
+    reward:"৳500",
+    category:"Casino",
+    image:"https://placehold.co/600x350/111827/22c55e?text=BC+Game",
+    link:"https://bc.game"
+},
+
+{
+    id:2,
+    title:"1xBet Bonus",
+    description:"প্রথম ডিপোজিটে বিশেষ বোনাস।",
+    reward:"৳1000",
+    category:"Sports",
+    image:"https://placehold.co/600x350/111827/22c55e?text=1xBet",
+    link:"https://1xbet.com"
+},
+
+{
+    id:3,
+    title:"MelBet Offer",
+    description:"আজকের Exclusive Bonus Offer।",
+    reward:"৳700",
+    category:"Betting",
+    image:"https://placehold.co/600x350/111827/22c55e?text=MelBet",
+    link:"https://melbet.com"
+}
+
+];
